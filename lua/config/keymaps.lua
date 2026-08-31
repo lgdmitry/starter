6db0cc57ec04c77f86b445daaf6f96159ffe46c8
pesotsky <pesotsky.d@gmail.com>
2026-08-31 11:02:24 +0300

    feat: клавиши для копирования пути и имени файла в буфер обмена
    <leader>cp — полный путь, <leader>cf — имя файла, <leader>cr — относительный путь.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cp", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = "Copy full path" })
+vim.keymap.set("n", "<leader>cf", function() vim.fn.setreg("+", vim.fn.expand("%:t")) end, { desc = "Copy filename" })
+vim.keymap.set("n", "<leader>cr", function() vim.fn.setreg("+", vim.fn.expand("%")) end, { desc = "Copy relative path" })
